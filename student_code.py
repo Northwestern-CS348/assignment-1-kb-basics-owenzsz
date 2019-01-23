b6b7f5427ab67648bb0b349fc70a0f9f909224d6
Owen Zhang <owenzhang@dhcp-10-105-200-136.wireless.northwestern.private>
2019-01-22 22:04:29 -0600

    Assignment 1

--- student_code.py
+++ student_code.py
@@ -23,6 +23,14 @@ class KnowledgeBase(object):
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
+        if isinstance(fact, Fact):
+            if fact not in self.facts:
+                self.facts.append(fact)
+                # if the fact/rule is already in the list, do not append
+                #if the input is a fact, append to the fact list
+        if isinstance(fact, Rule):
+            if fact not in self.rules:
+                self.rules.append(fact) #if it's a rule, append it to the rule list
         print("Asserting {!r}".format(fact))
         
     def kb_ask(self, fact):
@@ -34,4 +42,11 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
+        ans = ListOfBindings()
+        arr = []
+        for existingFact in self.facts:
+            if match(existingFact.statement, fact.statement)!=False: #find if there is any match in the current facts
+                arr.append(existingFact) #if yes, append it to the empty array for future use
+                ans.add_bindings(match(existingFact.statement, fact.statement), arr) #append the matches to the listofbindings
+        return ans
         print("Asking {!r}".format(fact))
